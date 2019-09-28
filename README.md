2914aff282a7de516d99efd53028ff311d7cb74f
Cecilia Leon <leoncecilia17@gmail.com>
2019-09-28 18:04:13 -0500

    set up client folder

--- README.md
+++ README.md
@@ -2,7 +2,14 @@
 
 ## Description 
 
+This is a memory game
+
 ## Usage 
 
+
+## Technology Used 
+
+
 ## Deployed Link 
-Please click on the following 
+Please click on the following link to view the most recently deployed version: 
+https://leoncecilia17.github.io/clickygame/
--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import DogCard from "./components/DogCard";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import ScoreBoard from "./components/ScoreBoard"; 
-import dogs from "./dogbreeds.json";
+import dogs from "./dogbreeds.json.js";
 
 class App extends Component {
   // Setting this.state.friends to the friends json array
