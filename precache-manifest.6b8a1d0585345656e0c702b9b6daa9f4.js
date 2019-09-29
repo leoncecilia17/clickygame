6d4d3b843d41dabdbaa08134c974c3911bf4dc16
leoncecilia17 <leoncecilia17@gmail.com>
2019-09-28 22:48:57 -0500

    Updates

---
+++ precache-manifest.6b8a1d0585345656e0c702b9b6daa9f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da25d8a6f02acdf32ab95761df18562e",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "91e3ff40aad3d3130efd",
+    "url": "/clickygame/static/css/main.682b219f.chunk.css"
+  },
+  {
+    "revision": "f5f73154a1956c611bf8",
+    "url": "/clickygame/static/js/2.365bb623.chunk.js"
+  },
+  {
+    "revision": "91e3ff40aad3d3130efd",
+    "url": "/clickygame/static/js/main.feca549a.chunk.js"
+  },
+  {
+    "revision": "dd4c8c421a5bb6fe730a",
+    "url": "/clickygame/static/js/runtime~main.eb7eb10a.js"
+  }
+]);
